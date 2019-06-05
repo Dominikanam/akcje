b3424fd0a90fbbea678c35dbc89d0c44d14c0409
Dominika Myszczynska <myszczynskad@gmail.com>
2019-06-05 15:53:04 +0100

    functions done

---
+++ actions.js
@@ -0,0 +1,38 @@
+import uuid from uuid;
+import { ADD_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT } from './actionTypes';
+
+const addComment = (text) => ({
+	type: ADD_COMMENT,
+    text,
+    id: uuid.v4()
+});
+
+const removeComment = (id) => ({
+	type: REMOVE_COMMENT,
+	id
+});
+
+const editComment = (id, text) => ({
+	type: EDIT_COMMENT,
+	id,
+	text
+});
+
+const thumbUpComment = (id) => ({
+	type: THUMB_UP_COMMENT,
+	id
+});
+
+const thumbDownComment = (id) => ({
+	type: THUMB_DOWN_COMMENT,
+	id
+});
+
+function users(state = [], action) {
+	switch (action.type) {
+	case 'NEW_USER':
+	  return [...state, action.name]
+	default:
+	  return state;
+	}
+}
